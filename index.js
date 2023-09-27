49123f7596ddff469a06ffe18d00ddfe4da14249
Leon Kinyanjui Maingi <leon.kinyanjui@student.moringaschool.com>
2023-09-27 14:03:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(record) {
+    return record.find(team => team.result === 'W').year; 
+  }
+
+  function superbowlWin(record) {
+    const win = record.find(team => team.result === 'W');
+  
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
+  
